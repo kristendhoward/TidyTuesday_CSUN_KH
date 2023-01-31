b243386d668876b4f6905fd70d915a05c5d38705
Kristen Howard <kristen.howard.739@my.csun.edu>
2023-01-31 15:20:45 -0800

    Commit a data

--- testscript.R
+++ testscript.R
@@ -1,1 +1,15 @@
-#
+# Kristen Howard
+# 1/31/2023
+# Power point on slide 45/54
+# This is my script
+
+# In the terminal
+# First you have to PULL
+# Type git pull in the terminal
+# Type git add testscript.r
+
+# Here is how to commit.
+# Type: git commit -a -m "This is how you commit"
+
+#Now we will push
+# git push
