8851f583017f37b6790e3d0aa5a21b5b53d0ef64
Kristen Howard <kristen.howard.739@my.csun.edu>
2023-02-13 22:24:12 -0800

    Added Hollywood Age Gap Data and organized script. Descriptive Stats or Analysis has not been started

---
+++ 2023/Hollywood Age Gaps/Script/Hollywood_Age_Gaps.R
@@ -0,0 +1,19 @@
+###############
+# Kristen Howard
+# Tidy Tuesday February 14, 2023
+#
+
+### Load Packages ###
+library(tidyverse)
+library(tidytuesdayR)
+library(lubridate)
+library(ggplot2)
+library(tidyverse)
+library(here)
+
+###Read Data ###
+
+glimpse("movies.csv")
+View("movies.csv")
+
+### Data Analysis ###
