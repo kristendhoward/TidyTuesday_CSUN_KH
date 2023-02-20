a76c653b168cbbd172cdc61456ec9af9ee8d7374
Kristen Howard <kristen.howard.739@my.csun.edu>
2023-02-19 21:25:44 -0800

    Tidy Tuesday. Experimenting with brewer color scales. Help is needed.

--- 2023/Bobb Ross Paintings/Scipts/Bob_Ross_Script.R
+++ 2023/Bobb Ross Paintings/Scipts/Bob_Ross_Script.R
@@ -1,8 +1,9 @@
-########################
+##############################
 # Kristen Howard
+# Bobby Ross Paintings
 # Tidy Tuesday February 14, 2023
 #Created on February 15th, 2023
-
+###############################
 ##### packages ####
 # devtools::install_github("frankiethull/BobRossColors")
 devtools::install_github("tidyverse/tidyverse")
@@ -11,7 +12,7 @@ library(dplyr)
 library(tidyverse)
 library(here)
 library(tidytuesdayR)
-library(BobRossColors)
+library(viridis)
 
 ##### Read Data ####
 bob_ross <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-21/bob_ross.csv')
@@ -19,16 +20,41 @@ View(bob_ross)
 
 write.csv(bob_ross,here::here("2023", "Bobb Ross Paintings", "Data","bob_ross.csv"))
 #Save the data to the Data folder like so! It worked
-glimpse(bob_ross)
+glimpse(bob_ross) # Look into data
 head(bob_ross)
 
 ##### Data Analysis ####
 bob_ross %>%
-  ggplot(aes(x = `season`,
+  ggplot(aes(x = `season`,  #Label x and y
              y = `num_colors`)) +
   geom_point(size = 3) +
-  scale_color_bob_ross(painting = c("peaceful_valley","a_walk_in_the_woods"), type = "qualitative")
-  labs( x = "Season", y = "Number of Colors", Title = "Number of Colors Used Through Out Seasons")
+  scale_color_continuous(type = "viridis") + #choosing this color package
+  labs( x = "Season", y = "Number of Colors") + #label axises
+  ggtitle(wrapper(my_title, width = 70)) #Create title
 
+my_title <- "Number of Colors Through Out the Seasons"
+wrapper <- function(x, ...)  #This title is long so create a wrapper function
+{paste(strwrap(x, ...), collapse = "\n")}
 
-  install.packages("scales")
+#Heat data Map
+ggp <- ggplot(bob_ross, aes(season, num_colors)) +  #Set X and Y                   
+  geom_tile(aes(fill = num_colors)) + scale_fill_continuous(type = "viridis") +
+  ggtitle(wrapper(my_title, width = 70))
+ggp
+
+ggp_binned <- ggplot(bob_ross, aes(season, num_colors)) +  #Set X and Y                   
+  geom_tile(aes(fill = num_colors)) + scale_fill_binned(type = "viridis") +
+  ggtitle(wrapper(my_title, width = 70))
+ggp_binned
+
+ggp_binned <- ggplot(bob_ross, aes(season, num_colors)) +  #Set X and Y                   
+  geom_tile(aes(fill = num_colors)) + scale_color_brewer(palette = "Oranges") +
+  ggtitle(wrapper(my_title, width = 70))
+ggp_binned
+
+########################
+#Trying to calculate mean
+bob_ross_mean <- bob_ross %>%
+                    group_by(num_colors,season) %>%
+                    summarise(parameters_mean = mean(num_colors, .groups = 'drop'))
+
